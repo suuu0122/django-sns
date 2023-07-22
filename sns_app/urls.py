fadf7bb7c9c3169a3f8797661f4dad85111e6340
suuu0122 <suuu0122lab@gmail.com>
2023-07-22 20:41:46 +0900

    added first signup

--- sns_app/urls.py
+++ sns_app/urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from . import views
 
 
 
+app_name = "sns_app"
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('signup/', views.signup, name="signup"),
 ]
