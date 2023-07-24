984af8e6ab497e2417eb823628d6b21d6c352a72
suuu0122 <suuu0122lab@gmail.com>
2023-07-24 21:18:54 +0900

    added Tweet model

--- sns_app/models.py
+++ sns_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Tweet(models.Model):
+    title       = models.CharField(max_length=100)
+    content     = models.TextField()
+    contributor = models.TextField()
+    sns_image   = models.ImageField(upload_to='')
+    good        = models.IntegerField()
+    read        = models.IntegerField()
+    read_text   = models.TextField()
