3174ac48e20a6ea6bd207647821f5dacbe48ab77
suuu0122 <suuu0122lab@gmail.com>
2023-07-24 15:10:48 +0900

    changed to prevent duplicate user registration

--- sns_app/views.py
+++ sns_app/views.py
@@ -1,6 +1,16 @@
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.shortcuts import render
 
 
 
 def signup(request):
-    return render(request, "sns_app/signup.html", {})
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        try:
+            user = User.objects.create_user(username=username, password=password)
+            return render(request, "sns_app/signup.html")
+        except IntegrityError:
+            return render(request, "sns_app/signup.html", {"error": "このユーザはすでに登録されています"})
+    return render(request, "sns_app/signup.html")
